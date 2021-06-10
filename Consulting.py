8dbf5c2de3f9f209db2feb366d6ff922666e722f
JCH <jc1997hdez@gmail.com>
2021-06-10 11:40:41 -0400

    add new router

--- Consulting.py
+++ Consulting.py
@@ -20,14 +20,14 @@ class RouterNode:
     def BindPort(self,port):
         context = zmq.Context()
         socket = context.socket(zmq.REP)
-        socket.connect("tcp://localhost:%s" % port)
+        socket.bind("tcp://localhost:%s" % port)
         # context = zmq.Context()
         # socket = context.socket(zmq.REP)
         # socket.bind("tcp://*:%s" % port)   
         return socket
 
 
-    def SaveClient(self,socket,url):
+    def SaveClient(self, address, url):
         print("Recibi la url en SaveClient 1")
         with threading.Lock():
             print("Recibi la url en SaveClient 1121")
@@ -35,16 +35,16 @@ class RouterNode:
             print("Recibi la url en SaveClient 2")
             try:
                 print("Recibi la url en SaveClient 3")
-                self.clients[hashedUrl].append(socket)
+                self.clients[hashedUrl].append(address)
             except KeyError:
                 print("como no tego la url voy a crear la nueva lista")
-                self.clients[hashedUrl] = [socket]
+                self.clients[hashedUrl] = [address]
                 r = self.LookUrlInChord(hashedUrl,url)
                 self.ProccessReplay(hashedUrl,r)
 
-    def LookUrlInChord(slef,hashedUrl,url):
+    def LookUrlInChord(self, hashedUrl, url):
         try:
-            id = hashedUrl%2**5
+            id = hashedUrl % 2 ** 5
             entry_point = Pyro4.Proxy(f"PYRONAME:Node.{8}")#aki hay q poner mas de uno por si falla
             chord_node_id = entry_point.LookUp(id)
             chord_node_with_html = Pyro4.Proxy(f"PYRONAME:Node.{chord_node_id}")
@@ -56,42 +56,38 @@ class RouterNode:
             #probar otro entry point
             pass
 
-    def ProccessReplay(self,hashedUrl,result):
+    def ProccessReplay(self, hashedUrl, result):
         print("hasta aki llego")
         with threading.Lock():
             for s in self.clients[hashedUrl]:
                 s.send_json({'data':'hola'})
 
-    def RecvMesgWorker(self, c):
+    def RecvMesgWorker(self):
         while 1:
             with threading.Lock():
                 try:
-                    data = c.recv_string()
+                    data = self.socket.recv()
                     self.tcp_queue.put(data)
                 except:
                     print("error al recibir msg")
 
-    def SenderWorker(self,socket):
+    def SenderWorker(self):
         while True:
-            url = self.tcp_queue.get()
-            print(url)
-            self.SaveClient(socket, url)
+            if tcp_queue.size() > 0:
+                url = self.tcp_queue.get()
+                print(url)
+                self.SaveClient(url)
 
-    def InitiateToSend(self,sckToPush,thread_worker_numbers=1):
+    def InitiateToSend(self, thread_worker_numbers = 1):
         for _ in range(thread_worker_numbers):
-            t =  threading.Thread(target=self.SenderWorker,args=(sckToPush,))
-            t.setDaemon(1)
-            t.start()
+            threading.Thread(target = self.SenderWorker, daemon = True).start()
 
-    def InitiateToRecv(self,sckForClients,thread_worker_numbers=1):
+    def InitiateToRecv(self, thread_worker_numbers = 1):
         for _ in range(thread_worker_numbers):
-            t =  threading.Thread(target=self.RecvMesgWorker,args=(sckForClients,))
-            t.setDaemon(1)
-            t.start()
-
+            threading.Thread(target = self.RecvMesgWorker, daemon = True).start()
 
     def Start(self):   
-        self.InitiateToRecv(self.socket)
+        self.InitiateToRecv()
         self.InitiateToSend(self.socket)
         while 1:
             if threading.activeCount() > 1:
