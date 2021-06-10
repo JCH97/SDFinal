83f4f24423e5ab8dc47e58faf5d277ffdeee7bae
Abel <abelo08cruz@gmail.com>
2021-06-10 14:27:28 -0400

    working project with multithreading implemented in router

--- Consulting.py
+++ Consulting.py
@@ -1,108 +1,98 @@
 import Pyro4
-import zmq
+# import zmq
 from Pyro4.errors import PyroError, CommunicationError
+import time
 import threading
-from queue import Queue
-import socket
+import zmq
+# import threading
+# from queue import Queue
+# import socket
 
-url = 'example.com'
+# url = 'example.com'
 
-# clients={}
-# replay_lock = threading.Lock()
+# # clients={}
+# # replay_lock = threading.Lock()
 
 class RouterNode:
     def __init__(self,port):
-        self.clients = {}
-        self.replay_lock = threading.Lock()
-        self.socket = self.BindPort(port)
-        self.tcp_queue = Queue()
+        self.BuildConn(port)
     
-    def BindPort(self,port):
-        context = zmq.Context()
+    def BuildConn(self,port):
+        """Server routine"""
+
+        url_worker = "inproc://workers"
+        url_client = f"tcp://*:{port}"
+
+        # Prepare our context and sockets
+        context = zmq.Context.instance()
+
+        # Socket to talk to clients
+        clients = context.socket(zmq.ROUTER)
+        clients.bind(url_client)
+
+        # Socket to talk to workers
+        workers = context.socket(zmq.DEALER)
+        workers.bind(url_worker)
+
+        # Launch pool of worker threads
+        for i in range(5):
+            thread = threading.Thread(target=self.worker_routine, args=(url_worker,))
+            thread.daemon = True
+            thread.start()
+
+        zmq.proxy(clients, workers)
+
+        # We never get here but clean up anyhow
+        clients.close()
+        workers.close()
+        context.term()
+
+    def worker_routine(self, worker_url, context=None):
+        """Worker routine"""
+        context = context or zmq.Context.instance()
+        # Socket to talk to dispatcher
         socket = context.socket(zmq.REP)
-        socket.connect("tcp://localhost:%s" % port)
-        # context = zmq.Context()
-        # socket = context.socket(zmq.REP)
-        # socket.bind("tcp://*:%s" % port)   
-        return socket
+
+        socket.connect(worker_url)
+
+        while True:
+
+            url  = socket.recv_string()
+
+            print("Received request: [ %s ]" % (url))
+            
+            # do some 'work'
+            r = self.SaveClient(url)
+            print(r)
+            #send reply back to client
+            socket.send_json(r)
 
 
-    def SaveClient(self,socket,url):
-        print("Recibi la url en SaveClient 1")
-        with threading.Lock():
-            print("Recibi la url en SaveClient 1121")
-            hashedUrl = hash(url)
-            print("Recibi la url en SaveClient 2")
-            try:
-                print("Recibi la url en SaveClient 3")
-                self.clients[hashedUrl].append(socket)
-            except KeyError:
-                print("como no tego la url voy a crear la nueva lista")
-                self.clients[hashedUrl] = [socket]
-                r = self.LookUrlInChord(hashedUrl,url)
-                self.ProccessReplay(hashedUrl,r)
-
-    def LookUrlInChord(slef,hashedUrl,url):
+    def SaveClient(self,url):
+        hashedUrl = hash(url)
+        try:
+            return self.LookUrlInChord(hashedUrl,url) 
+        except :
+            print("Error")
+            
+                
+    def LookUrlInChord(self,hashedUrl,url):
         try:
             id = hashedUrl%2**5
             entry_point = Pyro4.Proxy(f"PYRONAME:Node.{8}")#aki hay q poner mas de uno por si falla
             chord_node_id = entry_point.LookUp(id)
             chord_node_with_html = Pyro4.Proxy(f"PYRONAME:Node.{chord_node_id}")
             c = chord_node_with_html.GetUrl(url)
-            print('url q kiero buscar ' + url)
             return c
         except CommunicationError:
             print('ERRRRRROORRRRR')
             #probar otro entry point
             pass
 
-    def ProccessReplay(self,hashedUrl,result):
-        print("hasta aki llego")
-        with threading.Lock():
-            for s in self.clients[hashedUrl]:
-                s.send_json({'data':'hola'})
-
-    def RecvMesgWorker(self, c):
-        while 1:
-            with threading.Lock():
-                try:
-                    data = c.recv_string()
-                    self.tcp_queue.put(data)
-                except:
-                    print("error al recibir msg")
-
-    def SenderWorker(self,socket):
-        while True:
-            url = self.tcp_queue.get()
-            print(url)
-            self.SaveClient(socket, url)
-
-    def InitiateToSend(self,sckToPush,thread_worker_numbers=1):
-        for _ in range(thread_worker_numbers):
-            t =  threading.Thread(target=self.SenderWorker,args=(sckToPush,))
-            t.setDaemon(1)
-            t.start()
-
-    def InitiateToRecv(self,sckForClients,thread_worker_numbers=1):
-        for _ in range(thread_worker_numbers):
-            t =  threading.Thread(target=self.RecvMesgWorker,args=(sckForClients,))
-            t.setDaemon(1)
-            t.start()
-
-
-    def Start(self):   
-        self.InitiateToRecv(self.socket)
-        self.InitiateToSend(self.socket)
-        while 1:
-            if threading.activeCount() > 1:
-                continue
-            else:
-                break
 
 def main():
-    r = RouterNode(5560)
-    # tcp_queue = Queue()
-    r.Start()
+    r = RouterNode(5555)
 
 if __name__ == '__main__':
     main()#ver si hacen falata argumentos, como el puerto
+
